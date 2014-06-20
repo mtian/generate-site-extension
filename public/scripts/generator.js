320c877c782022546e0387f64bccc45c5dff3c6e
Mingwei Tian <mwtian@live.com>
2014-06-19 23:59:01 -0700

    Basic api.

--- public/scripts/generator.js
+++ public/scripts/generator.js
@@ -1,3 +1,17 @@
 (function() {
-    
+    $('#finish').click(function() {
+        var ext = {
+            'id': $('#ext-id').val(),
+            'title': $('#ext-name').val(),
+            'version': $('#ext-version').val(),
+            'authors': $('#ext-authors').val(),
+            'description': $('#ext-description').val(),
+            'iconurl': $('#ext-iconurl').val(),
+            'projecturl': $('#ext-projecturl').val(),
+            'licenseurl': $('#ext-licenseurl').val(),
+            'download_script': $('#download-script').val(),
+            'install_script': $('#install-script').val()
+        };
+        $.post("/", ext);
+    });
 })();
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ var path = require('path');
 
 /* GET home page. */
 var html_dir = './public/html/';
+
 router.get('/', function(req, res) {
   res.sendfile(path.join(html_dir, 'index.html'));
+});
+
+router.post('/', function(req, res) {
+  console.log(req);
+  res.send("OK");
 });
 
 module.exports = router;
